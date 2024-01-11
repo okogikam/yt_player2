ba847a87130a31988c19aa78c025be29e29d6e7b
okogikam <okogikam@gmail.com>
2024-01-12 00:18:35 +0800

    add viewpage

--- js/video.js
+++ js/video.js
@@ -1,22 +1,35 @@
 class Video{
     constructor(conf){
-        this.v = conf;
+        // this.v = conf;
+        this.title = conf.title.runs[0].text || "";
+        this.id = conf.videoId;
+        this.img = conf.thumbnail.thumbnails[0].url || "";
+        this.chanel = conf.ownerText.runs[0].text || "";
+        this.uploaded = conf.publishedTimeText.simpleText || "";
+        this.duration = conf.lengthText.simpleText || "";
+        this.chanImg = conf.channelThumbnailSupportedRenderers.channelThumbnailWithLinkRenderer.thumbnail.thumbnails[0].url || "";
     }
 
     display(){
         let vDiv = document.createElement("div");
         vDiv.classList.add("video");
         vDiv.innerHTML = `
-        <div class="card mb-3">
+        <div class="card mb-3" title="${this.title}">
             <div class="row g-0">
                 <div class="col-5">
-                    <img src="${this.v.thumbnail.thumbnails[0].url}" alt="" class="img-fluid rounded-start">
+                    <a href="/view/?id=${this.id}">
+                     <img src="${this.img}" alt="" class="img-fluid rounded-start">
+                     </a>
                 </div>
                 <div class="col-7">
                     <div class="card-body p-1">
-                        <a class="video-title" href="#">${this.v.title.runs[0].text}</a>
-                        <a class="chanel-title" href="#">${this.v.ownerText.runs[0].text}</a><br>
-                        <span class="btn btn-sm">${this.v.publishedTimeText.simpleText}</span>
+                        <a class="video-title" href="/view/?id=${this.id}">${this.title}</a>
+                        <a class="chanel-title" href="#">
+                        <img src="${this.chanImg}" class="chanel-img">${this.chanel}
+                        </a>
+                        <br>
+                        <span class="btn btn-sm">${this.duration}</span>
+                        <span class="btn btn-sm float-end">${this.uploaded}</span>
                     </div>
                 </div>
             </div>
--- js/viewPage.js
+++ js/viewPage.js
@@ -1,79 +1,34 @@
 class viewPage{
     constructor(conf){
-        this.Ytvideo = conf.Ytvideo;
-        this.idVideo = conf.idVideo;
+        this.vidId = conf.id;
+        this.url = `https://api.hancau.net/v2/?type=viewpage&v=${this.vidId}`;
+        this.loaded = false;
+        this.element = conf.element;
     }
-    next_video(data){
-        let videoList = this.Ytvideo.element.querySelector("#videoList");
-        videoList.innerHTML = "<h3>Watch Next</h3>";
-        data.forEach(vid => {
-            let btn = document.createElement("div");
-            btn.setAttribute("class","col-12");
-            btn.innerHTML = `
-            <div class="card aside m-2" title="${vid.title.simpleText}">
-              <div class="row">
-                <div class="col-4 p-0 m-0">
-                    <img src="${vid.thumbnail.thumbnails[0].url}" alt="">
-                    </div>
-                    <div class="col-8">
-                    <p class="no-wrap title">${vid.title.simpleText}</p>
-                    <p class="channel">
-                    <img src="${vid.channelThumbnail.thumbnails[0].url}">
-                    ${vid.longBylineText.runs[0].text.slice(0,15)}
-                    <span class="durasi"></span>
-                    </p>
-                </div>
-              </div>
-            </div>`;
-            videoList.appendChild(btn);
-            btn.querySelector(".card").addEventListener("click",()=>{
-                videoList.innerHTML = "";
-                let detail = this.Ytvideo.element.querySelector(".video-detail");
-                detail.innerHTML = ``;
-                this.Ytvideo.player.init({
-                    type: "playVideo",
-                    videoId: vid.videoId? vid.videoId : vid.playlistId
-                })
-                this.Ytvideo.saveHistory({
-                  videoId: vid.videoId? vid.videoId : vid.playlistId,
-                  title: vid.title.simpleText,
-                  thumbnail: vid.thumbnail.thumbnails[0].url,
-                  channel: vid.longBylineText.runs[0].text
-                })
-                
-                this.Ytvideo.displayViewPage(vid.videoId);
-            })   
-        });  
+    async load(){
+      let data = await fetch(this.url);
+      this.dataJson = await data.json();
+      this.loaded = true;
     }
-    videos(data){
-        if(!data){return}
-        let detail = this.Ytvideo.element.querySelector(".video-detail");
-        detail.innerHTML = `
-        <div class="card p-2 mb-3">
-          <h3 class="title">${data.title[0].runs[0].text}</h3>
-          <div class="channel-info mt-2 mb-2">
-             <div class="row">
-               <div class="col-6">
-                 <img src="${data.owner[0].videoOwnerRenderer.thumbnail.thumbnails[0].url}" >
-                 <span>@${data.owner[0].videoOwnerRenderer.title.runs[0].text}</span>
-               </div>
-               <div class="col-6 text-right">
-               <p>${data.dateText[0].simpleText}</p>
-               </div>
-             </div>
-          </div>
-          <div class="diskripsi text-justify">
-            <p>${data.attributedDescription[0].content}</p>
-          </div>
-        </div>
-        `
+    playVideo(){
+        let div = document.createElement("div");
+        div.classList.add("frameVideo");
+        div.innerHTML = `
+        <iframe id="player" type="text/html"
+  src="http://www.youtube.com/embed/${this.vidId}?enablejsapi=1&autoplay=1"
+  frameborder="0"></iframe>`
+
+        this.element.appendChild(div);
+    }
+    videos(){
+       
     }
     async display(){
-        let data = await fetch(`${this.Ytvideo.url}?type=viewpage&v=${this.idVideo}`);
-        let dataJson = await data.json();
-        let c = this.Ytvideo.element.querySelector("#videoList");
-        c.innerHTML = "";
-        this.videos(dataJson.videos);
-        this.next_video(dataJson.next_video);        
+        await this.load();
+
+        if(this.loaded){
+          this.element.innerHTML = "";
+          this.playVideo();
+        }       
     }
 }
