9b162768a194288077cd0ed107aafbaf4010d2d8
okogikam <83770559+okogikam@users.noreply.github.com>
2024-01-12 11:23:58 +0800

    Update viewPage.js

--- js/viewPage.js
+++ js/viewPage.js
@@ -14,9 +14,7 @@ class viewPage{
         let div = document.createElement("div");
         div.classList.add("frameVideo");
         div.innerHTML = `
-        <iframe id="player" type="text/html"
-  src="http://www.youtube.com/embed/${this.vidId}?enablejsapi=1&autoplay=1&origin=https://okogikam.github.io/yt_player2/"
-  frameborder="0"></iframe>`
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/${this.vidId}?si=-gWDYU0RPKmksWg1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
 
         this.element.appendChild(div);
     }
